18fa0927b26d59c5cc3a96ada67157d61642cf93
quantumblin <gogasarus@gmail.com>
2026-07-14 11:36:15 +0300

    daniln-network-websockets-js: Реализовал MockWebSockets для имитирования работы сервера.

--- public/scripts/utils/Network.js
+++ public/scripts/utils/Network.js
@@ -1,6 +1,7 @@
 const RECONNECT_INTERVAL_MS = 3000;
+const USE_MOCK = true;
 
-class Network {
+export class Network {
     constructor(url) {
         this.url = url;
 
@@ -13,7 +14,7 @@ class Network {
     }
 
     connect() {
-        this.socket = new WebSocket(this.url);
+        this.socket = USE_MOCK ? new MockWebSocket(this.url) : new WebSocket(this.url);
 
         this.socket.onopen = () => this.openConnection();
 
@@ -31,6 +32,7 @@ class Network {
 
         if (this.reconnectTimer) {
             clearInterval(this.reconnectTimer);
+            this.reconnectTimer = null;
         }
     }
 
@@ -66,7 +68,7 @@ class Network {
     }
 
     send(type, payload) {
-        if (!this.isConnected || !this.socket) return;
+        if (!this.socket || this.socket.readyState !== WebSocketState.OPEN) return;
 
         const message = JSON.stringify({type, payload});
 
@@ -75,10 +77,91 @@ class Network {
         console.log('Данные успешно отправлены на сервер: ', message);
     }
 
+}
+
+const WebSocketState = Object.freeze({
+    CONNECTING: 0,
+    OPEN: 1,
+    CLOSING: 2,
+    CLOSED: 3
+});
+
+class MockWebSocket {
+    constructor(url) {
+        this.url = url;
+
+        this.readyState = WebSocketState.CONNECTING;
+
+        setTimeout(() => {
+            this.readyState = WebSocketState.OPEN;
+            if (this.onopen) {
+                this.onopen();
+            }
+
+            this.sendServerMessage({ type: 'init', payload: {uuid: 123}});
+        }, 100);
+    }
+
+    send(data) {
+        const parsedData = JSON.parse(data);
+
+        console.log('[SERVER] Фронтенд отправил', data);
+
+        setTimeout(() => this.methodController(parsedData), 50);
+
+    }
+
+    methodController(parsedData) {
+        switch (parsedData.type) {
+            case 'join_lobby':
+                console.log('[SERVER] Игрок присоединился к лобби.');
+                this.sendServerMessage({
+                    type: 'JOINED_LOBBY',
+                    payload: {session_id: 'id1234'}
+                });
+                break;
+            case 'ready':
+                console.log('[SERVER] Игрок готов к игре.');
+                this.sendServerMessage({
+                    type: 'spawn',
+                    payload: {x: '100', y: '100'}
+                });
+                break;
+            case 'reload':
+                console.log('[SERVER] Игрок перезаряжается.');
+                this.sendServerMessage({
+                    type: 'reloaded',
+                    payload: {bullets: 30}
+                });
+                break;
+            case 'shot':
+                console.log('[SERVER] Выстрел произошёл, угол - ', parsedData.payload.angle);
+                break;
+            case 'move':
+                console.log('[SERVER нажаты клавиши', parsedData.payload.keyboard, ', угол игрока: ', parsedData.payload.angle);
+                break;
+            default:
+                console.warn('[SERVER] Получена неизвестная команда!');
+        }
+    }
 
 
+    sendServerMessage(obj) {
+        if (this.onmessage && this.readyState === WebSocketState.OPEN) {
+            this.onmessage({data: JSON.stringify(obj)});
+        }
+    }
+
+    close() {
+        this.readyState = WebSocketState.CLOSING;
+
+        setTimeout(() => {
+            this.readyState = WebSocketState.CLOSED;
+            if (this.onclose) {
+                this.onclose();
+            }
+        }, 50);
+    }
 
 
 }
-
-export default Network
